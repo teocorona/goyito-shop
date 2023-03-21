9950320b82f04234aa58001d19424e16da867d59
teocorona <teo.corona.t@gmail.com>
2023-03-20 18:35:03 -0600

    add basic layout and light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import 'components/styles/globals.css'
+import { lightTheme } from 'components/themes'
 import type { AppProps } from 'next/app'
+// import { Roboto_Flex } from 'next/font/google'
+
+// const roboto = Roboto_Flex({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
